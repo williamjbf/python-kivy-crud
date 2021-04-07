b541dcbe193e0b2372de95eb750b527915dcaf52
William <williamjbf@gmail.com>
2021-04-07 15:39:34 -0300

    primeiro teste com o Kivy

---
+++ gui/teste.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+
+
+class Principal(Widget):
+    pass
+
+class Secundario(Widget):
+    pass
+
+class Teste(App):
+    def build(self):
+        return Principal()
+
+Teste().run()
